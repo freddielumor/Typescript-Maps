d77fba0e628717c7fbf7bea9f35b82e42ecba870
Freddie Lumor <freddielumor@gmail.com>
2020-09-07 20:35:28 +0100

    Initial commit
    - Basic setup
    - Add company class
    - Add user class
    - Initialise map

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import { User } from "./User";
+// import { Company } from "./Company";
+
+// Initialise new google map
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
