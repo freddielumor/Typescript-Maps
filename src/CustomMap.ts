22757a3e3dfffe8a23bb46ba1a2106df80a7dc5d
Freddie Lumor <freddielumor@gmail.com>
2020-09-20 16:40:17 +0100

    Refactor CustomMap marker method

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,7 +1,10 @@
-import { User } from "./User";
-import { Company } from "./Company";
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
-// Private CustomMap class
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -15,22 +18,12 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
